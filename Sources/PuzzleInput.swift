186b094c54100852cbffe067b93d922b10b96183
anders pedersen <andersfriis91@gmail.com>
2024-12-04 21:40:38 +0100

    remove puzzle input

--- Sources/PuzzleInput.swift
+++ Sources/PuzzleInput.swift
@@ -5,14 +5,9 @@ struct PuzzleInput {
     let part2: String
 
     init(day: String) throws {
-        guard let firstPart = Bundle.module.url(forResource: "\(day)-01", withExtension: "txt") else { throw URLError(.fileDoesNotExist) }
-        guard let secondPart = Bundle.module.url(forResource: "\(day)-02", withExtension: "txt") else { throw URLError(.fileDoesNotExist) }
+        guard let firstPart = Bundle.module.url(forResource: "part1", withExtension: "txt") else { throw URLError(.fileDoesNotExist) }
+        guard let secondPart = Bundle.module.url(forResource: "part2", withExtension: "txt") else { throw URLError(.fileDoesNotExist) }
         self.part1 = try String(contentsOf: firstPart, encoding: .utf8)
         self.part2 = try String(contentsOf: secondPart, encoding: .utf8)
-    }
-
-    init(part1: String? = nil, part2: String? = nil) {
-        self.part1 = part1 ?? "NOT SET"
-        self.part2 = part2 ?? "NOT SET"
     }
 }
